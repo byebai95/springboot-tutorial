f203cc00492d88b9857711870205146045996c66
baizhuangzhuang <15217742393@163.com>
2023-07-14 22:36:56 +0800

    [update] 删除 git .idea 文件

--- p08-aop/src/main/java/app/AnnotationApp.java
+++ p08-aop/src/main/java/app/AnnotationApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppRunner {
+public class AnnotationApp {
     public static void main(String[] args) {
-        SpringApplication.run(AppRunner.class, args);
+        SpringApplication.run(AnnotationApp.class, args);
     }
 }
